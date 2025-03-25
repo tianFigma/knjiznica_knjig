230470f2f8189f6b6b9da6ba425262d5e77d7f9f
Tian <tian.pilaj@scv.si>
2025-03-25 18:18:59 +0100

    user

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
+    before_action :authenticate_user!
     belongs_to :user
     has_many :book_categories
     has_many :categories, through: :book_categories
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
+    before_action :authenticate_user!
     has_many :book_categories
     has_many :books, through: :book_categories
   end
